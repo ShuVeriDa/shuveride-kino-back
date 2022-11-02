701d087735579c8b79312489ca99e921110315bb
ShuVeriDa <bashtarov@outlook.com>
2022-11-02 17:35:24 +0300

    added model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from "nestjs-typegoose";
-import { ModelType } from "@typegoose/typegoose/lib/types";
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from 'nestjs-typegoose'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { UserModel } from '../user/user.model'
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(UserModule) private readonly UserModel: ModelType<UserModel>) {
-  }
+  constructor(
+    @InjectModel(UserModel)
+    private readonly UserModel: ModelType<UserModel>
+  ) {}
 
   async register(dto: any) {
-    return this.
+    const newUser = new this.UserModel(dto)
+    return newUser.save()
   }
 }
---
+++ src/user/user.model.ts
@@ -0,0 +1,18 @@
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses'
+import { prop } from '@typegoose/typegoose'
+
+export type UserModelType = Base
+
+export class UserModel extends TimeStamps {
+  @prop({ unique: true })
+  email: string
+
+  @prop()
+  password: string
+
+  @prop({ default: false })
+  isAdmin: boolean
+
+  @prop({ default: [] })
+  favorites?: []
+}
